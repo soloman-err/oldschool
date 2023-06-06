c05c139c4fb9eb5313283d300051fcb3699094c6
solo <soloman.err@.com>
2023-06-06 23:02:31 +0600

    Scheme: created responsive navbar

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from '../shared/navbar/Navbar';
 
 const Root = () => {
     return (
         <div>
+            <Navbar/>
             <Outlet></Outlet>
         </div>
     );
