d390a8ed2a50f9cb4881d0618759c27c75e88d29
solo <soloman.err@.com>
2023-06-06 23:13:00 +0600

    Scheme: Added react-icons package

--- src/shared/navbar/Navbar.jsx
+++ src/shared/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { FaBars } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
@@ -15,20 +15,7 @@ const Navbar = () => {
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
+           <FaBars size={20}/>
           </label>
           <ul
             tabIndex={0}
