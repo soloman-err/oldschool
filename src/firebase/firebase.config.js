624042ce6ca2d8a4ae125c48c25ef5b7222f9df7
solo <soloman.err@.com>
2023-06-07 02:01:00 +0600

    Deploy: added firebase configuration for live host & Authentication

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBq3jYCDwZMvx1XS1ot1ADSq3LZsOEl_uM",
+  authDomain: "oldschool-edu.firebaseapp.com",
+  projectId: "oldschool-edu",
+  storageBucket: "oldschool-edu.appspot.com",
+  messagingSenderId: "999786518241",
+  appId: "1:999786518241:web:6ed38b0d972bbbb04509a0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -2,12 +2,13 @@ import { Outlet } from 'react-router-dom';
 import Navbar from '../shared/navbar/Navbar';
 
 const Root = () => {
-    return (
-        <div className='relative'>
-            <Navbar/>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div className="relative">
+      <Navbar />
+
+      <Outlet></Outlet>
+    </div>
+  );
 };
 
 export default Root;
