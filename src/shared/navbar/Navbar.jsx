e20f22f69e45d9288b351233edfe8c3a0810eace
solo <soloman.err@.com>
2023-06-07 09:49:22 +0600

    Feature: conditional navigation system implemented to the navbar

--- src/shared/navbar/Navbar.jsx
+++ src/shared/navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import { FaBars } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-// import { schoolLogo } from "../../../src/assets/old-school-logo.png";
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
   const navigators = [
@@ -11,8 +10,10 @@ const Navbar = () => {
     { name: 'Blog', to: '/blog' },
   ];
 
+  const location = useLocation();
+
   return (
-    <div className="navbar flex justify-between md:px-10 items-center shadow-lg">
+    <div className="navbar flex justify-between md:px-10 items-center shadow-lg rounded-lg">
       <div className="flex">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost md:hidden">
@@ -23,21 +24,41 @@ const Navbar = () => {
             className="menu menu-sm dropdown-content mt-3 p-2 space-y-3 shadow-lg rounded w-52 uppercase font-semibold bg-base-100"
           >
             {navigators.map((navitem, index) => (
-              <Link key={index} to={`${navitem?.to}`}>
+              <Link
+                key={index}
+                to={navitem.to}
+                className={`tracking-wider ${
+                  location.pathname === navitem.to
+                    ? 'underline underline-offset-4'
+                    : ''
+                }`}
+              >
                 {navitem?.name}
               </Link>
             ))}
           </ul>
         </div>
         <div>
-        {/* <Link to={"/"} className="text-xl md:text-3xl lg:text-4xl font-bold">Oldschool.</Link> */}
-        <img src={"../../../src/assets/old-school-logo.png"} alt="" className="w-16 h-16" />
+          {/* <Link to={"/"} className="text-xl md:text-3xl lg:text-4xl font-bold">Oldschool.</Link> */}
+          <img
+            src={'../../../src/assets/old-school-logo.png'}
+            alt=""
+            className="w-16 h-16"
+          />
         </div>
       </div>
       <div className="hidden md:flex">
         <ul className="menu menu-horizontal gap-6 uppercase px-1 xl:text-lg font-bold">
           {navigators.map((navitem, index) => (
-            <Link key={index} to={`${navitem?.to}`} className='tracking-wider'>
+            <Link
+              key={index}
+              to={navitem.to}
+              className={`tracking-wider ${
+                location.pathname === navitem.to
+                  ? 'underline underline-offset-4'
+                  : ''
+              }`}
+            >
               {navitem?.name}
             </Link>
           ))}
